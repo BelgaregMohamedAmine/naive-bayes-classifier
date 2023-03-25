00ae95af00d0f9224c9e4d11113808965a7f5aec
belgareg mohamed amine <belgaregmohamedamine0268@gmail.com>
2023-03-25 13:38:51 +0100

    second commit

--- spam_classifier.py
+++ spam_classifier.py
@@ -3,8 +3,15 @@ import io
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 
-# quelques variables globales utiles
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# chemin des dossiers contenant les fichiers
 PATH_TO_HAM_DIR = "D:/naive-bayes-spam-classifier-machine-learning/emails/ham"
 PATH_TO_SPAM_DIR = "D:/naive-bayes-spam-classifier-machine-learning/emails/spam"
 
@@ -24,11 +31,12 @@ def readFilesFromDirectory(path, classification):
         message = extract_mail_body(current_file)
         X.append(message)
         Y.append(classification)
-       
+        
            
 #fonction de lecture du contenu d'un fichier texte donne.
 #ici, on fait un peu de traitement pour ne prendre en compte que le "corps du mail".
 # On ignorer les en-tetes des mails
+
 def extract_mail_body(file_name_str):
     inBody = False
     lines = []
@@ -46,21 +54,100 @@ def extract_mail_body(file_name_str):
 readFilesFromDirectory(PATH_TO_HAM_DIR, HAM_TYPE)
 readFilesFromDirectory(PATH_TO_SPAM_DIR, SPAM_TYPE)
 
-training_set = pd.DataFrame({'X': X, 'Y': Y})
+# Diviser les données en deux parties: une pour l'apprentissage et une pour le test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.9)
 
-
-#------------------
-
-
+# Convertir les données textuelles en vecteurs numériques
 vectorizer = CountVectorizer()
-counts = vectorizer.fit_transform(training_set['X'].values)
+X_train_counts = vectorizer.fit_transform(X_train)
 
+# Appliquer la méthode de Naive Bayes sur le jeu de formation
 classifier = MultinomialNB()
-targets = training_set['Y'].values
-classifier.fit(counts, targets)
+classifier.fit(X_train_counts, Y_train)
+
+# Convertir les exemples de test en vecteurs numériques et les utiliser pour prédire les étiquettes
+X_test_counts = vectorizer.transform(X_test)
+predictions = classifier.predict(X_test_counts)
+
+# Afficher les prédictions pour chaque exemple de test
+# for i in range(len(X_test)):
+#     print("Example: ", X_test[i])
+#     print("Prediction: ", predictions[i])
+#     print("True Label: ", Y_test[i])
 
 
-examples = ['Free Viagra now!!!', "Hi Bob, how about a game of golf tomorrow?"]
-example_counts = vectorizer.transform(examples)
-predictions = classifier.predict(example_counts)
-print (predictions)
+# Prédiction pour les exemples de test
+test_counts = vectorizer.transform(X_test)
+predictions = classifier.predict(test_counts)
+
+# Création du tableau de prédictions
+pred_df = pd.DataFrame({
+    'Email': X_test,
+    'Prediction': predictions,
+    'True Label': Y_test
+})
+
+# Affichage du tableau de prédictions
+print(pred_df)
+
+
+
+# Prédiction pour les exemples de test
+test_counts = vectorizer.transform(X_test)
+predictions = classifier.predict(test_counts)
+
+# Calcul du score de prédiction
+score = classifier.score(test_counts, Y_test)
+
+# Affichage du score
+print("Score de prédiction : {:.2f}%".format(score * 100))
+
+
+
+
+# Prédiction pour les exemples de test
+test_counts = vectorizer.transform(X_test)
+predictions = classifier.predict(test_counts)
+
+# Création d'un dataframe pour les prédictions incorrectes
+df_incorrect = pd.DataFrame(columns=["Email", "Prediction", "True Label"])
+
+# Boucle pour remplir le dataframe avec les prédictions incorrectes
+for i in range(len(X_test)):
+    if predictions[i] != Y_test[i]:
+        df_incorrect = df_incorrect.append({
+            "Email": X_test[i],
+            "Prediction": predictions[i],
+            "True Label":  Y_test[i]
+        }, ignore_index=True)
+
+# Affichage du dataframe
+print(df_incorrect)
+
+
+# Prédiction pour les exemples de test
+test_counts = vectorizer.transform(X_test)
+predictions = classifier.predict(test_counts)
+
+# Création du tableau de prédictions
+pred_df = pd.DataFrame({
+    'Email': X_test,
+    'Prediction': predictions,
+    'True Label': Y_test
+})
+
+# Affichage du tableau de prédictions
+print(pred_df)
+
+# Création de la matrice de confusion
+cm = confusion_matrix(Y_test, predictions, labels=[SPAM_TYPE, HAM_TYPE])
+
+# Création de la heatmap de la matrice de confusion
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=[SPAM_TYPE, HAM_TYPE], yticklabels=[SPAM_TYPE, HAM_TYPE])
+plt.xlabel('Prédiction')
+plt.ylabel('Vraie étiquette')
+plt.title('Matrice de confusion')
+plt.show()
+
+
+
